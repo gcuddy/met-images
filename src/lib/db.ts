e0c6a65050ea2a79c3f852718031ca60b4f3996a
Gus <gus.cuddy@gmail.com>
2021-09-22 00:01:39 -0400

    removed supabase

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-	import.meta.env.VITE_SUPABASE_URL,
-	import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+// const supabase = createClient(
+// 	import.meta.env.VITE_SUPABASE_URL,
+// 	import.meta.env.VITE_SUPABASE_ANON_KEY
+// );
 
-export default supabase;
+// export default supabase;
